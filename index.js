f7c88f592ee58c4c6c6d6563e120110aab26b19f
gcampbell391 <gcampbell391@gmail.com>
2020-03-28 20:41:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(value) {
+    let result;
+
+    if (value > 2499) {
+        result = "No can do."
+    } else if (value > 2000) {
+        result = "I will gladly take your thirty bucks."
+    } else if (value < 401) {
+        result = 'This one is on me!';
+    }
+    return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(value) {
+    return (value === "NYC" ? 'Ok, sounds good.' : 'No go.');
+    // Write your code here!
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(value) {
+    let result;
+    switch (value) {
+        case 'generous':
+            result = "Thank you so much."
+            break;
+        case 'not as generous':
+            result = "Thank you."
+            break;
+        case 'thanks for everything':
+            result = "Bye."
+            break;
+    }
+    return result;
+
+
 }
